5a5c6be647e65d39d68e0c2b1f1cad59f73c8e63
Jorge Luiz <j87150712@gmail.com>
2024-11-25 12:50:27 -0300

    ADS-tabela_criptomoedas: 01666811 React-vite

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,28 @@ function App() {
 
   return (
     <>
+      <main>
+        <table>
+          <tr>
+            <th scope="col">Criptomoedas</th>
+            <th scope="col">Preço (USD)</th>
+            <th scope="col"><button onClick={ordem}>Variação 24h (%) {ordemcrescente ? <BsCaretUpFill />  : <BsFillCaretDownFill /> } </button></th>
+          </tr>
+        
+          
+          {ordemoedas.map((moeda) => (
+            <tr key={moeda.id}>
+              <td><img src={moeda.image} alt={"icone_moeda"} width={15}/>{moeda.name}({moeda.symbol})</td>
 
+              <td>$ {moeda.current_price.toFixed(2)}</td>
+
+              <td style={{color: moeda.price_change_percentage_24h < 0 ? 'red' : 'Lime'}}>{moeda.price_change_percentage_24h.toFixed(2)}%</td>
+              
+            </tr>
+          ))}
+        
+        </table>
+      </main>
     </>
   )
 }
