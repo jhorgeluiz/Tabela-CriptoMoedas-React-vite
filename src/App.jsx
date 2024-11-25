83d6cd69202105d92b3c5de2edcee5911a1d2e4f
Jorge Luiz <j87150712@gmail.com>
2024-11-25 13:01:01 -0300

    ADS-ordenacao_colunas: 01666811 Reactjs-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { BsFillCaretDownFill, BsCaretUpFill} from "react-icons/bs"
+import { useState, useEffect } from 'react'
+import './App.css'
 import './App.css'
 
 function App() {
+  const [moeda, setmoeda] = useState([])
+  const [valor, setvalor] = useState([])
+  const [ordemcrescente, setordemcrescente] = useState(true)
 
+  const campo = (event) => {
+    setvalor(event.target.value)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchApi()
+      setmoeda(data)
+    }
+    fetchData()
+  })
+
+  const ordem = () => {
+    setordemcrescente(!ordemcrescente)
+  }
+
+  const ordemoedas = moeda
+  .filter((moeda) =>  moeda.name.includes(valor) || moeda.symbol.includes(valor))
+
+
+  .sort((a, b) => {
+    if (ordemcrescente) {
+      return a.price_change_percentage_24h - b.price_change_percentage_24h;
+    }
+    return b.price_change_percentage_24h - a.price_change_percentage_24h;
+  })
+
+  
+  const fetchApi = async () => {
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
+    const data = await res.json()
+    return data
+  }
   return (
     <>
       <header>
